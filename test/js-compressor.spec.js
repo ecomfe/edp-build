7ebe971ca0090a9d01a91a4568b2b199d819eadc
leeight <leeight@gmail.com>
2013-10-15 15:30:51 +0800

    恢复js-compressor中sourceMap的功能
    添加对应的测试用例

--- test/js-compressor.spec.js
+++ test/js-compressor.spec.js
@@ -1,21 +1,22 @@
 /***************************************************************************
- * 
+ *
  * Copyright (c) 2013 Baidu.com, Inc. All Rights Reserved
- * $Id$ 
- * 
+ * $Id$
+ *
  **************************************************************************/
- 
- 
- 
+
+
+
 /**
  * js-compressor.spec.js ~ 2013/09/28 19:39:59
  * @author leeight(liyubei@baidu.com)
- * @version $Revision$ 
- * @description 
- *  
+ * @version $Revision$
+ * @description
+ *
  **/
 var path = require('path');
 
+var ProcessContext = require('../lib/process-context.js');
 var JsCompressor = require('../lib/processor/js-compressor.js');
 var base = require('./base');
 
@@ -40,6 +41,25 @@ describe('js-compressor', function() {
         var fileData = base.getFileInfo(filePath);
         processor.process(fileData, null, function() {
             expect(fileData.data).toBe('function main(){function callSuper(){}var require=0,n=1,r=2,foobar=3;return callSuper(require+n+r+foobar)}');
+        });
+    });
+
+    it("支持设置sourceMapOptions", function() {
+        var processor = new JsCompressor({
+            sourceMapOptions: {
+                enable: true
+            }
+        });
+        var filePath = path.join('data', 'js-compressor', '5.js');
+        var fileData = base.getFileInfo(filePath);
+        var processContext = new ProcessContext( {
+            baseDir: 'src',
+            exclude: [],
+            outputDir: 'output',
+            fileEncodings: {}
+        } );
+        processor.process(fileData, processContext, function() {
+            expect(fileData.data).toBe('function main(){function n(){}var require=0,exports=1,module=2,r=3;return n(require+exports+module+r)}\n//# sourceMappingURL=5.sourcemap');
         });
     });
 });
