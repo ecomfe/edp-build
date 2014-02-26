0131da924759bb8fdf5f97eefeee566e6af28723
leeight <leeight@gmail.com>
2014-02-26 21:49:57 +0800

    Fix #30

--- test/get-module-id.spec.js
+++ test/get-module-id.spec.js
@@ -25,7 +25,7 @@ describe('get-module-id', function() {
     it('default', function() {
         var moduleFile = path.resolve(Project, 'src', 'foo.js');
         var moduleId = GetModuleId(moduleFile, ConfigFile);
-        expect(moduleId).toEqual('foo');
+        expect(moduleId).toEqual(['foo']);
         expect(GetModuleFile('foo', ConfigFile)).toEqual(moduleFile);
     });
 
@@ -33,12 +33,12 @@ describe('get-module-id', function() {
         // 项目内的依赖
         var moduleFile = path.resolve(Project, 'dep', 'er', '3.0.2', 'src', 'View.js');
         var moduleId = GetModuleId(moduleFile, ConfigFile);
-        expect(moduleId).toEqual('er/View');
+        expect(moduleId).toEqual(['er/View']);
         expect(GetModuleFile('er/View', ConfigFile)).toEqual(moduleFile);
 
         var mainModuleFile = path.resolve(Project, 'dep', 'er', '3.0.2', 'src', 'main.js');
         var mainModuleId = GetModuleId(mainModuleFile, ConfigFile);
-        expect(mainModuleId).toEqual('er');
+        expect(mainModuleId).toEqual(['er']);
         expect(GetModuleFile('er', ConfigFile)).toEqual(mainModuleFile);
     });
 
@@ -47,31 +47,40 @@ describe('get-module-id', function() {
         // data/base/io/1.0.0/src/File.js
         var ioModuleFile = path.resolve(Project, '..', 'base', 'io', '1.0.0', 'src', 'File.js');
         var ioModuleId = GetModuleId(ioModuleFile, ConfigFile);
-        expect(ioModuleId).toEqual('io/File');
+        expect(ioModuleId).toEqual(['io/File']);
         expect(GetModuleFile('io/File', ConfigFile)).toEqual(ioModuleFile);
 
         // data/base/net/1.2.0/src/Http.js
         var netModuleFile = path.resolve(Project, '..', 'base', 'net', '1.2.0', 'src', 'Http.js');
         var netModuleId = GetModuleId(netModuleFile, ConfigFile);
-        expect(netModuleId).toEqual('net/Http');
+        expect(netModuleId).toEqual(['net/Http']);
         expect(GetModuleFile('net/Http', ConfigFile)).toEqual(netModuleFile);
 
         var netMainModuleFile = path.resolve(Project, '..', 'base', 'net', '1.2.0', 'src', 'main.js');
         var netMainModuleId = GetModuleId(netMainModuleFile, ConfigFile);
-        expect(netMainModuleId).toEqual('net');
+        expect(netMainModuleId).toEqual(['net']);
         expect(GetModuleFile('net', ConfigFile)).toEqual(netMainModuleFile);
     });
 
     it('paths', function() {
         var moduleFile = path.resolve(Project, 'src', 'resource', 'css', 'base.css');
         var moduleId = GetModuleId(moduleFile, ConfigFile);
-        expect(moduleId).toEqual('css/base.css');
+        expect(moduleId).toEqual(['css/base.css', 'resource/css/base.css']);
         // FIXME(??) expect(GetModuleFile('css/base.css', ConfigFile)).toEqual(moduleFile);
 
         var jsModuleFile = path.resolve(Project, 'src', 'resource', 'css', 'base.js');
         var jsModuleId = GetModuleId(jsModuleFile, ConfigFile);
-        expect(jsModuleId).toEqual('css/base');
+        expect(jsModuleId).toEqual(['css/base', 'resource/css/base']);
         expect(GetModuleFile('css/base', ConfigFile)).toEqual(jsModuleFile);
+    });
+
+    it('edp-build/issues/30', function(){
+        var moduleFile = path.resolve(Project, 'src', 'common', 'ecma', 'tpl.js');
+        var moduleConfig = path.resolve(Project, 'issue-30-module.conf');
+        var moduleIds = GetModuleId( moduleFile, moduleConfig );
+
+        // 如果你好奇为啥多了一个'ecma/tpl'，其实我也很好奇，但是就是多了一个
+        expect( moduleIds ).toEqual( [ 'tpl', 'ecma/tpl', 'common/ecma/tpl' ] );
     });
 });
 
