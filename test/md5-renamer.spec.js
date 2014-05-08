e7d07287df585834cfbc10487c060b6dd538c8fd
leeight <leeight@gmail.com>
2014-05-08 21:47:16 +0800

    修复outputPaths的路径错误问题
    同时添加更多的测试用例
    ecomfe/edp#253

--- test/md5-renamer.spec.js
+++ test/md5-renamer.spec.js
@@ -116,19 +116,14 @@ describe( 'md5-renamer', function() {
         base.launchProcessors( processors, processContext, function(){
             done = true;
             runs(function(){
-                function x( a ) {
-                    return a.map(function( i ){
-                        return edp.path.join( Project, i );
-                    });
-                }
                 var f1 = processContext.getFileByPath( 'src/common/logo.gif' );
                 var f2 = processContext.getFileByPath( 'src/common/main.css' );
                 var f3 = processContext.getFileByPath( 'src/common/main.js' );
                 var k1 = processContext.getFileByPath( 'src/biz/foo.css' );
-                expect( f1.outputPaths ).toEqual( x( [ 'src/common/logo-ba001c53c2b.gif' ] ) );
-                expect( f2.outputPaths ).toEqual( x( [ 'src/common/main-5de4eeb0d4f.css' ] ) );
-                expect( f3.outputPaths ).toEqual( x( [ 'src/common/main-d299e81d71c.js' ] ) );
-                expect( k1.outputPaths ).toEqual( x( [ 'src/biz/foo-03b5a7b5a96.css' ] ) );
+                expect( f1.outputPaths ).toEqual( ( [ 'src/common/logo-ba001c53c2b.gif' ] ) );
+                expect( f2.outputPaths ).toEqual( ( [ 'src/common/main-5de4eeb0d4f.css' ] ) );
+                expect( f3.outputPaths ).toEqual( ( [ 'src/common/main-d299e81d71c.js' ] ) );
+                expect( k1.outputPaths ).toEqual( ( [ 'src/biz/foo-03b5a7b5a96.css' ] ) );
 
                 var f4 = processContext.getFileByPath( 'issue-235.html' );
                 expect( f4.data.indexOf( 'src/common/main-d299e81d71c.js?foo=bar#hello=world' ) ).not.toBe( -1 );
@@ -137,6 +132,10 @@ describe( 'md5-renamer', function() {
                 expect( f4.data.indexOf( '<script src="http://www.baidu.com/a.js?x=1"></script>' ) ).not.toBe( -1 );
                 expect( f4.data.indexOf( '<script src="//www.baidu.com/foo/bar.js?y=2"></script>' ) ).not.toBe( -1 );
                 expect( f4.data.indexOf( '<script src="https://www.google.com/ssl.js#a=b"></script>' ) ).not.toBe( -1 );
+                expect( f4.data.indexOf( '<embed src=\'src/common/logo-ba001c53c2b.gif\' widht=100 height=200 />' ) ).not.toBe( -1 );
+                expect( f4.data.indexOf( '<param value=\'src/common/logo-ba001c53c2b.gif\' name="movie" />' ) ).not.toBe( -1 );
+                expect( f4.data.indexOf( '<param value="src/common/logo.gif" />' ) ).not.toBe( -1 );
+
 
                 var f5 = processContext.getFileByPath( 'src/common/main.css' );
                 expect( f5.data.indexOf( 'logo-ba001c53c2b.gif' ) ).not.toBe( -1 );
