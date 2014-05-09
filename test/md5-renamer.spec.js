c1000f56616d5a2e265ceda8a4fd151b62c41e97
leeight <leeight@gmail.com>
2014-05-09 12:04:51 +0800

    支持只处理html中特定tag的功能
    如果设置了replacements，但是没有设置files，也保证MD5Renamer这个Processor
    可以正常工作，目的是为了减少配置的参数
    
    ecomfe/edp#253

--- test/md5-renamer.spec.js
+++ test/md5-renamer.spec.js
@@ -123,7 +123,7 @@ describe( 'md5-renamer', function() {
                 expect( f1.outputPaths ).toEqual( ( [ 'src/common/logo-ba001c53c2b.gif' ] ) );
                 expect( f2.outputPaths ).toEqual( ( [ 'src/common/main-5de4eeb0d4f.css' ] ) );
                 expect( f3.outputPaths ).toEqual( ( [ 'src/common/main-d299e81d71c.js' ] ) );
-                expect( k1.outputPaths ).toEqual( ( [ 'src/biz/foo-03b5a7b5a96.css' ] ) );
+                expect( k1.outputPaths ).toEqual( ( [ 'src/biz/foo-7506118fea0.css' ] ) );
 
                 var f4 = processContext.getFileByPath( 'issue-235.html' );
                 expect( f4.data.indexOf( 'src/common/main-d299e81d71c.js?foo=bar#hello=world' ) ).not.toBe( -1 );
@@ -141,9 +141,73 @@ describe( 'md5-renamer', function() {
                 expect( f5.data.indexOf( 'logo-ba001c53c2b.gif' ) ).not.toBe( -1 );
                 var f6 = processContext.getFileByPath( 'src/biz/foo.css' );
                 expect( f6.data.indexOf( '../common/logo-ba001c53c2b.gif' ) ).not.toBe( -1 );
+                expect( f6.data.indexOf( 'background: url(../common/logo-ba001c53c2b.gif)  ;' ) ).not.toBe( -1 );
+                expect( f6.data.indexOf( 'background: url("../common/logo-ba001c53c2b.gif");' ) ).not.toBe( -1 );
             });
         });
+    });
 
+    it( 'issue-235-1', function(){
+        // replacements不生效的问题
+        var p1 = new MD5Renamer({
+            outputTemplate: '{basename}-{md5sum}{extname}',
+            start: 1,
+            end: 12,
+            replacements: {
+                html: {
+                    tags: [
+                        { tag: 'img', attribute: 'src' }
+                    ],
+                    files: [ 'issue-235.html' ]
+                }
+            }
+        });
+        var processContext = new ProcessContext( {
+            baseDir: Project,
+            exclude: [],
+            outputDir: 'output',
+            fileEncodings: {}
+        });
+        base.traverseDir( Project, processContext );
+
+        var processors = [
+            p1
+        ];
+
+        var done = false;
+        waitsFor(function(){ return done; });
+        base.launchProcessors( processors, processContext, function(){
+            done = true;
+            runs(function(){
+                var f1 = processContext.getFileByPath( 'src/common/logo.gif' );
+                var f2 = processContext.getFileByPath( 'src/common/main.css' );
+                var f3 = processContext.getFileByPath( 'src/common/main.js' );
+                var k1 = processContext.getFileByPath( 'src/biz/foo.css' );
+                expect( f1.outputPaths ).toEqual( ( [ 'src/common/logo-ba001c53c2b.gif' ] ) );
+                expect( f2.outputPaths ).toEqual( ( [ ] ) );
+                expect( f3.outputPaths ).toEqual( ( [ ] ) );
+                expect( k1.outputPaths ).toEqual( ( [ ] ) );
+
+                var f4 = processContext.getFileByPath( 'issue-235.html' );
+                expect( f4.data.indexOf( 'src/common/main.js?foo=bar#hello=world' ) ).not.toBe( -1 );
+                expect( f4.data.indexOf( 'src/common/main.css' ) ).not.toBe( -1 );
+                expect( f4.data.indexOf( 'src/common/logo-ba001c53c2b.gif' ) ).not.toBe( -1 );
+                expect( f4.data.indexOf( '<script src="http://www.baidu.com/a.js?x=1"></script>' ) ).not.toBe( -1 );
+                expect( f4.data.indexOf( '<script src="//www.baidu.com/foo/bar.js?y=2"></script>' ) ).not.toBe( -1 );
+                expect( f4.data.indexOf( '<script src="https://www.google.com/ssl.js#a=b"></script>' ) ).not.toBe( -1 );
+                expect( f4.data.indexOf( '<embed src=\'src/common/logo.gif\' widht=100 height=200 />' ) ).not.toBe( -1 );
+                expect( f4.data.indexOf( '<param value=\'src/common/logo.gif\' name="movie" />' ) ).not.toBe( -1 );
+                expect( f4.data.indexOf( '<param value="src/common/logo.gif" />' ) ).not.toBe( -1 );
+
+
+                var f5 = processContext.getFileByPath( 'src/common/main.css' );
+                expect( f5.data.indexOf( 'logo.gif' ) ).not.toBe( -1 );
+                var f6 = processContext.getFileByPath( 'src/biz/foo.css' );
+                expect( f6.data.indexOf( '../common/logo.gif' ) ).not.toBe( -1 );
+                expect( f6.data.indexOf( 'background: url  (../common/logo.gif)  ;' ) ).not.toBe( -1 );
+                expect( f6.data.indexOf( 'background: url("../common/logo.gif");' ) ).not.toBe( -1 );
+            });
+        });
     });
 });
 
