be275f75d59a0ebbf4b729d616eb1ad5b9cc1ce6
leeight <leeight@gmail.com>
2015-05-15 20:58:05 +0800

    修复travis-ci上面的ut失败的问题

--- test/css-spriter.spec.js
+++ test/css-spriter.spec.js
@@ -1,32 +1,23 @@
-/***************************************************************************
- *
- * Copyright (c) 2014 Baidu.com, Inc. All Rights Reserved
- * $Id$
- *
- **************************************************************************/
-
-
-
 /**
- * css-spriter.spec.js ~ 2014/12/17 22:40:05
+ * @file css-spriter.spec.js ~ 2014/12/17 22:40:05
  * @author quyatong(quyatong@126.com)
- * @version $Revision$
- * @description
- *
- **/
+ */
 var fs = require('fs');
 var path = require('path');
-var CssSpriter = require('../lib/processor/css-spriter.js');
-var PathMapper = require('../lib/processor/path-mapper.js');
-var Project = path.resolve(__dirname, 'data', 'css-spriter');
+
 var base = require('./base');
 var ProcessContext = require('../lib/process-context');
+var CssSpriter = require('../lib/processor/css-spriter.js');
+var CssCompressor = require('../lib/processor/css-compressor.js');
 
-describe('css-spriter', function() {
-    it('default', function() {
+var Project = path.resolve(__dirname, 'data', 'css-spriter');
+
+describe('css-spriter', function () {
+    it('default', function (done) {
         var processor = new CssSpriter({
             files: ['src/*.css']
         });
+        var p2 = new CssCompressor();
 
         var processContext = new ProcessContext({
             baseDir: Project,
@@ -37,16 +28,22 @@ describe('css-spriter', function() {
 
         base.traverseDir(Project, processContext);
 
-        base.launchProcessors([processor], processContext, function() {
-            var cssData = processContext.getFileByPath('src/default.css').data;
-            var compareCssData = fs.readFileSync(path.join(__dirname, 'data/css-spriter/default.compare.css'), 'utf-8');
+        base.launchProcessors([processor, p2], processContext, function () {
+            var cssData = processContext.getFileByPath('src/default.css');
+            expect(cssData).not.toBeNull();
 
             var imgData = processContext.getFileByPath('src/sprite-default.png').data;
             var compareImgData = fs.readFileSync(path.join(__dirname, 'data/css-spriter/sprite-default.png'));
 
             // 去掉版本号比较
-            expect(cssData.replace(/\?ver=\d*/g, '')).toBe(compareCssData);
+            var expected =
+                'div{color:red;background:url(sprite-default.png#300*200)}\n' +
+                'a{text-decoration:none;background:url(./b.png#nocombine)no-repeat}\n' +
+                'span{background:url(sprite-default.png#271*134)-300px 0 no-repeat}';
+            expect(cssData.data.replace(/\?ver=\d*/g, '')).toBe(expected);
             expect(imgData).toEqual(compareImgData);
+
+            done();
         });
     });
 });
--- test/custom-processor.spec.js
+++ test/custom-processor.spec.js
@@ -1,44 +1,52 @@
-var builder = require('../index');
-var path = require('path');
+/**
+ * @file test/custom-processor.spec.js
+ * @author leeight
+ */
 var fs = require('fs');
+var path = require('path');
 
+var edp = require('edp-core');
 
-describe('custom-processor', function() {
-    it('raw object', function() {
+var builder = require('../index');
+
+describe('custom-processor', function () {
+    it('raw object', function (done) {
         var outputDir = path.resolve(__dirname, 'data/css-compressor/_output');
         var conf = {
             input: path.resolve(__dirname, 'data/css-compressor'),
             output: outputDir,
             getProcessors: function () {
                 return [
-                    { 
-                        name:'clearOutput', 
-                        process: function (file,context,callback) {
-                            if (file.extname =='less') {
+                    {
+                        name: 'clearOutput',
+                        process: function (file, context, callback) {
+                            if (file.extname === 'less') {
                                 file.outputPath = '';
                             }
                             callback();
                         }
                     }
-                ]
+                ];
             }
         };
 
         builder(conf, function () {
             expect(fs.existsSync(path.resolve(outputDir, '1.less'))).toBeFalsy();
             expect(fs.existsSync(path.resolve(outputDir, '1.less.html'))).toBeTruthy();
-            require( 'edp-core' ).util.rmdir( outputDir );
+            edp.util.rmdir(outputDir);
+            done();
         });
     });
 
-    it('inherits AbstractProcessor', function() {
+    it('inherits AbstractProcessor', function (done) {
         var conf = require('./custom-processor-config');
         var outputDir = conf.output;
 
         builder(conf, function () {
             expect(fs.existsSync(path.resolve(outputDir, 'default.js'))).toBeFalsy();
             expect(fs.existsSync(path.resolve(outputDir, '5.js'))).toBeTruthy();
-            require( 'edp-core' ).util.rmdir( outputDir );
+            edp.util.rmdir(outputDir);
+            done();
         });
     });
 
