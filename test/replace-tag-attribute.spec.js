2186261d4bef5129f2dded7d46ac434d01e70568
Firede <firede@firede.us>
2015-10-21 17:49:10 +0800

    issue #104. add test case && bugfix.

---
+++ test/replace-tag-attribute.spec.js
@@ -0,0 +1,55 @@
+/**
+ * @file 测试一下 replace-tag-attribute 功能是否正常
+ * @author Firede(firede@firede.us)
+ */
+
+var replaceTagAttribute = require('../lib/util/replace-tag-attribute');
+
+describe('replace-tag-attribute', function () {
+
+    var expectValue = '<head><script src="/root/path/file.js" type="text/javascript"></script></head>';
+
+    it('default', function () {
+        var value = replaceTagAttribute(
+            '<head><script src="${root}/path/file.js" type="text/javascript"></script></head>',
+            'script',
+            'src',
+            function (val) {
+                return val.replace('${root}', '/root');
+            }
+        );
+
+        expect(value).toBe(expectValue);
+    });
+
+    it('attribute value include `<`', function () {
+        var value = replaceTagAttribute(
+            '<head><script src="<$root>/path/file.js" type="text/javascript"></script></head>',
+            'script',
+            'src',
+            function (val) {
+                return val.replace('<$root>', '/root');
+            }
+        );
+        
+        expect(value).toBe(expectValue);
+    });
+
+    it('script-x should not matched', function () {
+        // FIXME 这个 case 暂时还通不过
+        // 当搜索 tag 为 script 时，会匹配到以 script 为前缀的所有 tag
+        // 要先确定当前有没有误用的情况，再修复此问题
+        var textScriptX = '<head><script-x src="${root}/path/file.js" type="text/javascript"></script></head>';
+        var value = replaceTagAttribute(
+            textScriptX,
+            'script',
+            'src',
+            function (val) {
+                return val.replace('${root}', '/root');
+            }
+        );
+        console.log(value);
+        expect(value).toBe(textScriptX);
+    });
+
+});
